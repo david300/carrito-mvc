fde6db97b7983ea76b648a33b16d0d3481309190
David Ferreira <davidferreira87@gmail.com>
2023-08-30 17:32:13 -0300

    abm inicio

---
+++ CapaDatos/CD_Usuarios.cs
@@ -0,0 +1,53 @@
+﻿using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class CD_Usuarios
+    {
+        public List<Usuario> Listar()
+        {
+            List<Usuario> lista = new List<Usuario>();
+            try
+            {
+                using (SqlConnection oConexion = new SqlConnection(Conexion.cn))
+                {
+                    string query = "select IdUsuario, Nombres, Apellidos, Correo, Clave, Restablecer, Activo, FechaRegistro from Usuario";
+                    SqlCommand cmd = new SqlCommand(query, oConexion);
+                    cmd.CommandType = CommandType.Text;
+
+                    oConexion.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while(dr.Read())
+                        {
+                            lista.Add(new Usuario() {
+                                IdUsuario = Convert.ToInt32(dr["IdUsuario"]),
+                                Nombres = dr["Nombres"].ToString(),
+                                Apellidos = dr["Apellidos"].ToString(),
+                                Correo = dr["Correo"].ToString(),
+                                Clave = dr["Clave"].ToString(),
+                                Restablecer = Convert.ToBoolean(dr["Restablecer"]),
+                                Activo = Convert.ToBoolean(dr["Activo"]),
+                                FechaRegistro = Convert.ToDateTime(dr["FechaRegistro"])
+                            });
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                lista = new List<Usuario>();
+            }
+
+            return lista;
+        }
+    }
+}
